581d5f55e3d8f6ddf000676fa5419aaf29c14215
AdrianJPT <86939628+AdrianJPT@users.noreply.github.com>
2023-07-20 14:41:36 -0500

    Update Credentials.java

--- src/test/java/Api_Netbox_Zabbix_Integration/Manage/Credentials.java
+++ src/test/java/Api_Netbox_Zabbix_Integration/Manage/Credentials.java
@@ -3,14 +3,14 @@ package Api_Netbox_Zabbix_Integration.Manage;
 public class Credentials {
 
     // MARK - Netbox Credentials
-    public String NetboxURL = "http://192.168.40.50:3000/";
-    public String NetboxUsername = "admin";
-    public String NetboxPassword = "admin";
+    public String NetboxURL = "http://4.228.209.251:8000/";
+    public String NetboxUsername = "Admin";
+    public String NetboxPassword = "Admin";
 
 
     // MARK - Zabbix Credentials
 
-    public String ZabbixURL = "http://192.168.40.50/";
+    public String ZabbixURL = "http://4.228.209.251/";
     public String ZabbixUsername = "Admin";
     public String ZabbixPassword = "zabbix";
 
